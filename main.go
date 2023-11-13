1d04c3361da4e1d994c5661499753bb6c4cb4312
YuKai Wang <55015205+johnson7543@users.noreply.github.com>
2023-11-13 11:41:08 +0800

    Update main.go

--- main.go
+++ main.go
@@ -78,7 +78,7 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     "http://1ocalhost:3000",
 		AllowMethods:     "GET,POST,PUT,PATCH,DELETE",
-  AllowHeaders:"Content-Type",
+ AllowHeaders:  "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
 		AllowCredentials: true,
 	}))
 
